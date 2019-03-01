5d8e50dcea7769bb13c0716e56d94a219786d438
Marc-Andre Quintal <45339322+maquintal@users.noreply.github.com>
2019-02-28 23:02:48 -0500

    add backup powershell script

---
+++ index.js
@@ -0,0 +1,14 @@
+var x;
+
+function myData() { 
+	x = "du texte";
+   return 123; 
+} 
+console.log(myData()); // 123 
+console.log(x);
+
+function myValue() { 
+} 
+console.log(myValue()); // undefined 
+
+console.log(x);

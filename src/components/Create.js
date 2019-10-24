11aa489cc8a38374d18ab2200e62aa31c2683892
Marc-Andre Quintal <45339322+maquintal@users.noreply.github.com>
2019-10-23 23:15:48 -0400

    css improvement

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,6 +1,7 @@
 // MODULES //
 import React from 'react';
 import { Formik, FieldArray, Field } from 'formik';
+//import { FormikConsumer } from 'formik';
 import firebase from '../Firebase';
 //import { Link } from 'react-router-dom';
 
@@ -8,14 +9,19 @@ import firebase from '../Firebase';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-//import Paper from '@material-ui/core/Paper';
+import Paper from '@material-ui/core/Paper';
 //import { Typography } from '@material-ui/core';
+/* import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select'; */
 
 // CUSTOM //
 import useStyles from '../css';
+import { Typography } from '@material-ui/core';
 
 const Create = (props) => {
-  console.log(props)
   const classes = useStyles();
   const ref = firebase.firestore().collection('recipes');
 
@@ -23,19 +29,35 @@ const Create = (props) => {
     <Formik
       {...props}
       initialValues={{ 
-        title: '',
+        name: '',
+        style: '',
+        brewer: '',
+        version: '',
         description: '',
-        author: '',
-        friends: [
-          {
-            name: "Klaus",
-            email: "klaus@formik.com"
-          },
-          {
-            name: "Hans",
-            email: "hans@formik.com"
-          }
-        ]
+        malts: [{
+          name: '',
+          quantity: '',
+        }],
+        waters: [{
+          step: '',
+          quantity: '',
+          duration: '',
+        }],
+        hops: [{
+          name: "",
+          quantity: ""
+        }],
+        yeasts: [{
+          name: '',
+          quantity: '',
+        }],
+        miscs: [{
+          name: '',
+          quantity: '',
+        }],
+        IBU: '',
+        SRM: '',
+        est_ABV: '',
       }}
       /*validate={values => {
         let errors = {};
@@ -51,46 +73,135 @@ const Create = (props) => {
       onSubmit={(values, form) => {
 
         ref.add({
-          title: values.title,
-          description: values.description,
-          friends: values.friends
-          //author: values.author
+          values
         }).then((docRef) => {
-          /* setState({
-            title: '',
-            description: '',
-            author: ''
-          }); */
           props.history.push("/")
         })
         .catch((error) => {
           console.error("Error adding document: ", error);
         });
         form.setSubmitting(false);
-      }}
-    //>
+      }}    
+    
     render={form => (
+      
       <form onSubmit={form.handleSubmit}>
         <div className={classes.root}>
-          <Grid container spacing={3}>
-            <Grid item xs={12}>
-              {/* <Paper className={classes.paper}> */}
-                <TextField
-                  id="title"
-                  name="title"
-                  label="Title"
-                  //class="form-control"
-                  //className={classes.textField}
-                  value={form.values.name}
-                  onChange={form.handleChange}
-                  margin="normal"
+          <br />
+          <React.Fragment>
+          <Paper className={classes.paper}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} className={classes.paperTitle}>
+                <Typography variant="h5">
+                  General Beer Recipe Information
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Field
+                  name="name"
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Name"
+                      placeholder="Red Fox"
+                      //class="form-control"
+                      className={classes.textField}
+                      onChange={form.handleChange}
+                      margin="normal"
+                    />
+                  )}
                 />
                 {/* errors.email && touched.email && errors.email */}
-              {/* </Paper> */}
+              </Grid>
+              <Grid item xs={3}>
+                <Field
+                  name="style"
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Style"
+                      placeholder="Red Ale"
+                      //class="form-control"
+                      className={classes.textField}
+                      onChange={form.handleChange}
+                      margin="normal"
+                    />
+                  )}
+                />
+                {/* errors.email && touched.email && errors.email */}
+              </Grid>
+              <Grid item xs={3}>
+                <Field
+                  name="brewer"
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Brewer"
+                      placeholder="Au Camp de Chasse"
+                      //class="form-control"
+                      className={classes.textField}
+                      onChange={form.handleChange}
+                      margin="normal"
+                    />
+                    /* <FormControl className={classes.formControl}>
+                      <InputLabel 
+                        htmlFor="age-simple"
+                        className={classes.select}
+                      >
+                        Age
+                      </InputLabel>
+                      <Select
+                        {...field}
+                        className={classes.select}                        
+                      >
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                      </Select>
+                  </FormControl> */
+                  )}
+                />
+                {/* errors.email && touched.email && errors.email */}
+              </Grid>
+              <Grid item xs={3}>
+                <Field
+                  name="version"
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Version"
+                      placeholder="1.1"
+                      //class="form-control"
+                      className={classes.textField}
+                      onChange={form.handleChange}
+                      margin="normal"
+                    />
+                  )}
+                />
+                {/* errors.email && touched.email && errors.email */}
+              </Grid>
             </Grid>
+          </Paper>
+          </React.Fragment>
+          
+          <br />
+          <br />
+          <React.Fragment>
+          <Paper className={classes.paper}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <Typography variant="h5">
+                  title
+                </Typography>
+              </Grid>
+            </Grid>
+          </Paper>
+          </React.Fragment>
+
+          <Grid container spacing={3}>
             <Grid item xs={12}>
-              {/* <Paper className={classes.paper}> */}
-                <TextField 
+               {/* <Paper className={classes.paper}> */}
+                {/* <TextField 
                   id="description"
                   name="description"
                   label="Description"
@@ -103,72 +214,86 @@ const Create = (props) => {
                   multiline
                   value={form.values.description}
                   margin="normal"
-                />
+                /> */}
                 {/* errors.password && touched.password && errors.password */}
               {/* </Paper> */}
             </Grid>
             <Grid item xs={12}>
             <FieldArray
-            name="friends"
+            name="malts"
             render={({ insert, remove, push }) => (
               <div>
-                {form.values.friends.length > 0 &&
-                  form.values.friends.map((friend, index) => (
-                    <div className="row" key={index}>
-                      <div className="col">
-                        <label htmlFor={`friends.${index}.name`}>Name</label>
-                        <Field
-                          name={`friends.${index}.name`}
-                          placeholder="Jane Doe"
-                          type="text"
-                        />
-                        {/* errors.friends &&
-                          errors.friends[index] &&
-                          errors.friends[index].name &&
-                          touched.friends &&
-                          touched.friends[index].name && (
-                            <div className="field-error">
-                              {/* errors.friends[index].name }
-                            </div>
+                {form.values.malts.length > 0 &&
+                  form.values.malts.map((malt, index) => (
+                     /* <div /* className="row" key={index}> */
+                      /* <div className="col"> */
+                      <Grid container spacing={8} direction="row" justify="space-around" key={index}>
+                        <Grid item xs={6} sm={3}>
+                          <Button
+                            type="button"
+                            className="secondary"
+                            onClick={() => push({ name: "", quantity: "" })}
+                          >
+                            Add Malt
+                          </Button>
+                        </Grid>
+                        <Grid item xs={6} sm={3}>
+                          <Field
+                            name={`malts.${index}.name`}
+                            render={({ field }) => (
+                              <TextField
+                                {...field}
+                                label="Malt Name"
+                                //class="form-control"
+                                className={classes.textField}
+                                //value={form.values.malts[index].name}
+                                onChange={form.handleChange}
+                                margin="normal"
+                              />
+                            )}
+                          />
+                          
+                          {/* errors.malts &&
+                            errors.malts[index] &&
+                            errors.malts[index].name &&
+                            touched.malts &&
+                            touched.malts[index].name && (
+                              <div className="field-error">
+                                {/* errors.malts[index].name }
+                              </div>
                           )*/}
-                      </div>
-                      <div className="col">
-                        <label htmlFor={`friends.${index}.email`}>
-                          Email
-                        </label>
-                        <Field
-                          name={`friends.${index}.email`}
-                          placeholder="jane@acme.com"
-                          type="email"
-                        />
-                        {/* errors.friends &&
-                          errors.friends[index] &&
-                          errors.friends[index].email &&
-                          touched.friends &&
-                          touched.friends[index].email && (
-                            <div className="field-error">
-                              {errors.friends[index].email}
-                            </div>
-                          ) */}
-                      </div>
-                      <div className="col">
-                        <button
-                          type="button"
-                          className="secondary"
-                          onClick={() => remove(index)}
-                        >
-                          X
-                        </button>
-                      </div>
-                    </div>
+                        </Grid>
+                        <Grid item xs={6} sm={3}>
+                          <label htmlFor={`malts.${index}.quantity`}>
+                            Quantity
+                          </label>
+                          <Field
+                            name={`malts.${index}.quantity`}
+                            placeholder="1000"
+                            type="string"
+                          />
+                          {/* errors.malts &&
+                            errors.malts[index] &&
+                            errors.malts[index].email &&
+                            touched.malts &&
+                            touched.malts[index].email && (
+                              <div className="field-error">
+                                {errors.malts[index].email}
+                              </div>
+                            ) */}
+                        </Grid>
+                        <Grid item xs={6} sm={3}>
+                          <button
+                            type="button"
+                            className="secondary"
+                            onClick={() => remove(index)}
+                          >
+                            X
+                          </button>
+                        </Grid>
+                      </Grid>
+                    /* </div> */
                   ))}
-                <button
-                  type="button"
-                  className="secondary"
-                  onClick={() => push({ name: "", email: "" })}
-                >
-                  Add Friend
-                </button>
               </div>
             )}
           />
@@ -185,7 +310,7 @@ const Create = (props) => {
           </Grid>
           {console.log(form.values)}
         </div>
-      </form>
+      </form>   
     )}
   />
   )
--- src/css.js
+++ src/css.js
@@ -2,13 +2,33 @@ import { makeStyles } from '@material-ui/core/styles';
 
 // CSS //
 const useStyles = makeStyles(theme => ({
+  
   root: {
     flexGrow: 1,
   },
+
+  // STRUCTURE //
   paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+    marginTop: theme.spacing(1),
+    //padding: theme.spacing(1),
+    paddingBottom: theme.spacing(7),
+    minWidth: "90%",
+    maxWidth: "90%",
+    marginLeft: theme.spacing(8),
+    //textAlign: 'center',
+    //color: theme.palette.text.secondary,
+  },
+  paperTitle: {
+    marginLeft: theme.spacing(2),
+  },
+
+  // FIELDS //
+  textField: {
+    marginLeft: theme.spacing(5),
+  },
+  select: {
+    paddingTop: theme.spacing(2),
+    minWidth: "200px",
   },
 }));
 
